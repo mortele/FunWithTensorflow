2cb1c615571ecae94d044f7a1d94f7d4ff479dbb
Morten Ledum <mortele@student.matnat.uio.no>
2016-09-21 10:09:36 +0200

    Changes?

--- QuantumTrainingData/scanEngradOrca.py
+++ QuantumTrainingData/scanEngradOrca.py
@@ -1,15 +1,17 @@
 import os
 import numpy 		as np
 
-nR 		= 1#int(1e5)
-r12 	= np.linspace(0.3, 	3.0, 	nR)
+nR 		= 100#int(1e5)
+r12 	= np.linspace(1.0,10.0, nR)
 
 def writeToInputFile(r) :
 	with open("input", "w") as inputFile :
 		inputFile.write("! DFT-Energy+ ENGRAD\n")
+		#inputFile.write("! UHF ENGRAD\n")
+		inputFile.write("! BOHRS\n")
 		inputFile.write("* xyz 0 1\n")
-		inputFile.write("  H 0 0 0\n")
-		inputFile.write("  H 0 0 %f\n" % (r))
+		inputFile.write("  O 0 0 0\n")
+		inputFile.write("  O 0 0 %f\n" % (r))
 		inputFile.write("*")
 
 def readFromOutputFile(inFileName) :
@@ -31,24 +33,32 @@ def readFromOutputFile(inFileName) :
 		g2[2] 	= float(lines[16].split('\n')[0])
 
 		r 		= lines[20].split('\n')[0].split()
-		print(r)
 		r1[0] 	= float(r[1])
 		r1[1] 	= float(r[2])
 		r1[2] 	= float(r[3])
 
 		r 		= lines[21].split('\n')[0].split()
-		print(r)
 		r1[0] 	= float(r[1])
 		r1[1] 	= float(r[2])
 		r1[2] 	= float(r[3])
 	return E,r1,r2,g1,g2
 
-for r in r12 :
-	writeToInputFile(r)
-	os.system("/Users/morten/Library/Orca/orca input > output")
+
+for i in range(len(r12)) :
+	print("Progress: %d of %d runs complete. r12=%f" % (i,nR,r12[i]))
+	writeToInputFile(r12[i])
+	if os.system("/Users/morten/Library/Orca/orca input > output") != 0 :
+		print("Orca run aborted. Exiting.")
+		exit()
+
 	E,r1,r2,g1,g2 = readFromOutputFile("input.engrad")
 
-	with open("qdata.dat", "a") as outFile :
+	fileFlag = None
+	if i==0 :
+		fileFlag = "w"
+	else :
+		fileFlag = "a"
+	with open("qdata.dat", fileFlag) as outFile :
 		outFile.write("%.15f %.15f %.15f %.15f %.15f %.15f %.15f %.15f %.15f %.15f %.15f %.15f %.15f\n" % \
 				(E, \
 				r1[0], r1[1], r1[2], \
